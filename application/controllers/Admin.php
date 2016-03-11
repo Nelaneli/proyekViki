1841520a52a93227d3e744c40183c29dbe24d210
Usaid Chaq <usaidelqudsy.chaq20@gmail.com>
2016-03-11 16:46:25 +0700

    tambah fungsi delete dan create

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,22 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->helper(array('url','form'));
+        $this->load->model('adminviki');
+    }
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
     public function index(){
         $this->load->view('login');
     }
@@ -68,12 +59,29 @@ class Admin extends CI_Controller {
         if($this->session->userdata('logged_in') == TRUE && $this->session->userdata('email_admin') == 'usaid_sc@yahoo.com' 
 		&& $this->session->userdata('password') == '123'){
             $this->load->view('header');
-            $this->load->view('forms');
+            $data['p'] = $this->adminviki->produk();
+            $this->load->view('forms',$data);
             $this->load->view('footer');
         }
         else{
             redirect('Admin');
 		}
+	}
+    public function add_produk(){
+        $this->load->model('adminviki');
+        $this->load->library('form_validation');
+        $a = $this->input->post('nama');
+        $b = $this->input->post('des');
+        $c = $this->input->post('hrg');
+        $this->adminviki->tambah_produk($a, $b, $c);
+        redirect(site_url('../index.php/admin/form'));
+    }
+    public function delete_produk($id)
+	{
+		$this->load->model('adminviki');
+		$this->adminviki->hapus_produk($id);		
+	
+		redirect(site_url('../index.php/admin/form'));	
 	}
     public function logout() {
         // Removing session data
