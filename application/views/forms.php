48276df199747c8a0a4b0fcadf6aee2f619205d0
Usaid Chaq <usaidelqudsy.chaq20@gmail.com>
2016-03-11 16:48:37 +0700

    membenarkan input dan view data

--- application/views/forms.php
+++ application/views/forms.php
@@ -21,13 +21,13 @@
                 <!-- /.row -->
 
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-lg-3">
 
-                        <form role="form">
+                        <form role="form" method="POST" enctype="multipart/form-data" action="<?php echo base_url();?>index.php/Admin/add_produk/" >
 
                             <div class="form-group">
                                 <label>Nama Produk</label>
-                                <input class="form-control">
+                                <input type="text" name="nama" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label>Foto</label>
@@ -36,13 +36,13 @@
 
                             <div class="form-group">
                                 <label>Deskripsi</label>
-                                <textarea class="form-control" rows="3"></textarea>
+                                <textarea class="form-control" rows="3" name="des"></textarea>
                             </div>
                             
                             <label>Harga</label>
                             <div class="form-group input-group">
                                 <span class="input-group-addon">Rp</span>
-                                <input type="number" class="form-control">
+                                <input type="number" class="form-control" name="hrg" required>
                                 <span class="input-group-addon">.00</span>
                             </div>
 
@@ -53,7 +53,7 @@
                         </form>
 
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-lg-9">
                         <h2>Daftar Produk</h2>
                         <div class="table-responsive">
                             <table class="table table-bordered table-hover table-striped">
@@ -65,25 +65,15 @@
                                         <th>Tindak Lanjut</th>
                                     </tr>
                                 </thead>
+                                <?php foreach ($p as $produk) { ?>
                                 <tbody>
                                     <tr>
-                                        <td>/index.html</td>
-                                        <td>1265</td>
-                                        <td>32.3%</td>
-                                        <td><button type="submit" class="btn btn-danger">Hapus</button></td>
+                                        <td><?php echo $produk->nama_produk; ?>. </td>
+                                        <td><?php echo $produk->hrg_produk; ?></td>
+                                        <td><?php echo $produk->desk_produk; ?> </td>
+                                        <td><a href="<?php echo base_url();?>index.php/Admin/delete_produk/<?php echo $produk->id_produk;?>" type="button" class="btn btn-danger">Hapus</a>	</td>
                                     </tr>
-                                    <tr>
-                                        <td>/about.html</td>
-                                        <td>261</td>
-                                        <td>33.3%</td>
-                                        <td><button type="submit" class="btn btn-danger">Hapus</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td>/sales.html</td>
-                                        <td>665</td>
-                                        <td>21.3%</td>
-                                        <td><button type="submit" class="btn btn-danger">Hapus</button></td>
-                                    </tr>
+                                    <?php } ?>
                                 </tbody>
                             </table>
                         </div>
